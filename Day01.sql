12391b57d1dfb33628befdfb82b32bd120f5668a
Soni Makwana <52577507+sonimakwana@users.noreply.github.com>
2021-05-07 12:30:22 -0500

    Update Day01.sql

--- Day01.sql
+++ Day01.sql
@@ -1,4 +1,12 @@
 Q1) https://leetcode.com/problems/combine-two-tables/
+
 Select FirstName, LastName, City, State
-From Person left join Address
-On Person.PersonID = Address.PersonID;
+From Person p left join Address a
+On p.PersonID = a.PersonID;
+
+
+Q2) https://leetcode.com/problems/second-highest-salary/submissions/
+
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE Salary < (SELECT MAX(Salary) FROM Employee);
